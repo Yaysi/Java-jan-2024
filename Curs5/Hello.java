af97b9af27477c2c391c1cb44c302435f3ccb947
Ismail <ismailtikhlal74@gmail.com>
2024-02-22 00:36:42 +0200

    Commited

--- Curs5/Hello.java
+++ Curs5/Hello.java
@@ -2,6 +2,13 @@ package Curs5;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hey");
+        int time = 20;
+        String result = (time > 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+
+        double Ismail_grade = 90;
+        String grade = (Ismail_grade >=90) ? "A" : "F";
+        System.out.println(grade);
     }
+
 }
--- Curs5/Login.java
+++ Curs5/Login.java
@@ -5,22 +5,24 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[] args) {
 
-        String username;
-        String password;
-        username = "Ismail";
-        password = "1234";
-
         Scanner in = new Scanner(System.in);
-        System.out.println(" Enter your user name : ");
-         username = in.next();
-        System.out.println("Enter your password : ");
-        password = in.next();
-        if (username.equals(username) && password.equals(password)) {
+        String sysusername = "Ismail";
+        String syspassword = "1234";
 
-            System.out.println("Access Granted! Welcome!");
-        } else if ((!username.equals(username) && !password.equals(password))) {
-            System.out.println("Invalid");
-            
+        System.out.println(" Enter your user name : ");
+        String username = in.next();
+        System.out.println("Enter your password : ");
+        String password = in.next();
+
+        if (sysusername.equals(username)){
+            if (syspassword.equals(password)){ System.out.println("Welcome Admin");
+        }else {
+                System.out.println("Please check your password");
+            }}
+        else {
+            System.out.println("Please check your user name");
+
         }
+
     }
 }

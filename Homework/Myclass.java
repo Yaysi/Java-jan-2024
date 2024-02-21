9437ee7386b986036d48e01c5aab3973b65110cb
Ismail <ismailtikhlal74@gmail.com>
2024-02-21 23:14:35 +0200

    Commited

--- Homework/Myclass.java
+++ Homework/Myclass.java
@@ -5,13 +5,32 @@ public class Myclass {
 
         Bloc bloc1= new Bloc(false,2011,4);
         Bloc bloc3= new Bloc(true,2020,5);
+        Bloc bloc4 = new Bloc(true,2026,7);
+        Bloc bloc5= new Bloc(true, 2026,7);
+
+
+        Masini car1 =new Masini(2014,"Silver","Mercedes");
+        Masini car2 = new Masini(2026,"Black", "Mercedes");
+        Masini car3= new Masini(2026, "Silver", "Mercedes");
+
+        Contracts contract1= new Contracts("Permanent", "Electronica", 5);
+
+
+
+
 
         if (bloc1.lift==false && bloc1.annee_de_construction<=2010){
             System.out.println("This bloc is good with the floor number "+bloc1.etaj);
         } else if (bloc1.annee_de_construction>2010) {
             System.out.println("Nothing");
 
+            if (car1.an_fabricatia<2023 && car1.model=="Mercedes")
+            {
+                System.out.println("This car is old");
+
+
+
         }
 
     }
-}
+}}
--- Main.java
+++ Main.java
@@ -2,12 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the length of a rectangle : ");
-        int length = in.nextInt();
-        System.out.println("Enter the breadth of a rectangle : ");
-        int breadth = in.nextInt();
-        int area = length*breadth;
-        System.out.println("The area of a rectangle is : "+area );
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter the length of a rectangle : ");
+//        int length = in.nextInt();
+//        System.out.println("Enter the breadth of a rectangle : ");
+ //       int breadth = in.nextInt();
+  //      int area = length*breadth;
+  //      System.out.println("The area of a rectangle is : "+area );
+
+        Scanner number = new Scanner(System.in);
+        System.out.println("Write your first number : ");
+        int number1 = number.nextInt();
+        System.out.println("Write your second number : ");
+        int number2 = number.nextInt();
+        double result = number1/number2;
+
+        System.out.println(" the result is "+ result);
+
+        if (number1 > 0) {
+            System.out.println(" this number is positive");}
+             else if (number1<0){
+                System.out.println("the number is negative " +number1);}
+
+             else System.out.println("the number = Zero");
+
     }
 }
